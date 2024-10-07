911a43320c89cd51ac5a98e55bb2081b2a303b93
Jacqui Bouchard <boucharj27@up.edu>
2024-10-07 15:40:43 -0700

    J: small comment

--- app/src/main/java/edu/up/cs301/mahjong/MahjongGameState.java
+++ app/src/main/java/edu/up/cs301/mahjong/MahjongGameState.java
@@ -46,7 +46,7 @@ public class MahjongGameState extends GameState {
 		this.isSet = false;
 		this.numSets = 0;
 		this.numPairs = 0;
-		this.currentHand = null;
+		this.currentHand = null; //need to copy each object to a new array when constructing, example on moodle
 		this.currentDrawnTile = null;
 		this.lastDiscarded = null;
 		this.deck = null;
