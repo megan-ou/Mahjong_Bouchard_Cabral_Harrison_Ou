5bd46cb508415574007551128c1f798636499f25
Jacqui Bouchard <boucharj27@up.edu>
2024-11-03 15:16:43 -0800

    JB: fixing comment from proj F feedback

--- app/src/main/java/edu/up/cs301/mahjong/MahjongHumanPlayer.java
+++ app/src/main/java/edu/up/cs301/mahjong/MahjongHumanPlayer.java
@@ -98,12 +98,6 @@ public class MahjongHumanPlayer extends GameHumanPlayer implements OnClickListen
 
 		MahjongGameState firstCopy = new MahjongGameState(firstInstance);
 
-
-		/**if (button instanceof MDButton) {
-			MahjongDiscardTileAction discardTile = new MahjongDiscardTileAction(this);
-			game.sendAction(discardTile);
-		} */ //from Lab 6
-
 		//Any text from previous run cleared
 		testResultsTextView.setText("");
 
