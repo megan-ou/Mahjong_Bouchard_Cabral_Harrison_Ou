50000cb18fbc8ea4fb83d4dd591e4e56316b2031
Jazmine Cabral <jhcabral07@gmail.com>
2024-11-07 14:07:13 -0800

    J.C.: MahjongGameStateTest copyArrayTest

--- app/src/androidTest/java/edu/up/cs301/mahjong/MahjongGameStateTest.java
+++ app/src/androidTest/java/edu/up/cs301/mahjong/MahjongGameStateTest.java
@@ -2,16 +2,50 @@ package edu.up.cs301.mahjong;
 
 import junit.framework.TestCase;
 
+import edu.up.cs301.mahjong.tiles.MahjongTiles;
+
 public class MahjongGameStateTest extends TestCase {
 
     public void setUp() throws Exception {
         super.setUp();
+
     }
 
     public void tearDown() throws Exception {
     }
 
-    public void testCopyArray() {
+    public void testCopyArray1() {
+        MahjongGameState state = new MahjongGameState();
+        MahjongTiles[] originalArray;
+        MahjongTiles[] copyArray;
+        originalArray = new MahjongTiles[]{
+                new MahjongTiles("bamboo", 2)
+        };
+        assertNotNull(originalArray);
+    }
+
+    public void testCopyArray2(){
+        MahjongGameState state = new MahjongGameState();
+        MahjongTiles[] originalArray;
+        MahjongTiles[] copyArray;
+        originalArray = new MahjongTiles[]{
+                new MahjongTiles("bamboo", 2),
+                new MahjongTiles("character", 5),
+                new MahjongTiles("circle", 7)
+        };
+        copyArray = new MahjongTiles[originalArray.length];
+
+        state.copyArray(copyArray, originalArray);
+
+        // Change on of the elements in the array to then get an error that it did not opy right
+        copyArray[0].setValue(5);
+        copyArray[0].setSuit("circle");
+
+        for (int i = 0; i < originalArray.length; i++) {
+            assertEquals(originalArray[i].getSuit(), copyArray[i].getSuit());
+            assertEquals(originalArray[i].getValue(), copyArray[i].getValue());
+            assertNotSame(originalArray[i], copyArray[i]);
+        }
     }
 
     public void testCopyArrayList() {
