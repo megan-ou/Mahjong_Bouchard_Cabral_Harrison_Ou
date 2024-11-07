4608c6a1dcf227f97dc3c7ed6adcf349b348c67a
Jazmine Cabral <jhcabral07@gmail.com>
2024-11-07 13:35:45 -0800

    J.C.: MahjongGameStateTest

---
+++ app/src/test/java/edu/up/cs301/mahjong/MahjongGameStateTest.java
@@ -0,0 +1,66 @@
+package edu.up.cs301.mahjong;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class MahjongGameStateTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void copyArray() {
+    }
+
+    @Test
+    public void copyArrayList() {
+    }
+
+    @Test
+    public void mahjongDeck() {
+    }
+
+    @Test
+    public void startGame() {
+    }
+
+    @Test
+    public void dealTiles() {
+    }
+
+    @Test
+    public void makeDiscardAction() {
+    }
+
+    @Test
+    public void makeDrawTileAction() {
+    }
+
+    @Test
+    public void makeChowAction() {
+    }
+
+    @Test
+    public void makeSwitchViewAction() {
+    }
+
+    @Test
+    public void testToString() {
+    }
+
+    @Test
+    public void deckToString() {
+    }
+
+    @Test
+    public void handToString() {
+    }
+}
