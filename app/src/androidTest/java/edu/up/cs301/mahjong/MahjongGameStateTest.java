cf8a4a44d7e4892705967198667b6144aafd4c63
meganou <ou27@up.edu>
2024-11-07 13:47:18 -0800

    MO - Redo test class

---
+++ app/src/androidTest/java/edu/up/cs301/mahjong/MahjongGameStateTest.java
@@ -0,0 +1,109 @@
+package edu.up.cs301.mahjong;
+
+import junit.framework.TestCase;
+
+public class MahjongGameStateTest extends TestCase {
+
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    public void tearDown() throws Exception {
+    }
+
+    public void testCopyArray() {
+    }
+
+    public void testCopyArrayList() {
+    }
+
+    public void testMahjongDeck() {
+    }
+
+    public void testStartGame() {
+    }
+
+    public void testDealTiles() {
+    }
+
+    public void testMakeDiscardAction() {
+    }
+
+    public void testMakeDrawTileAction() {
+    }
+
+    public void testMakeChowAction() {
+    }
+
+    public void testMakeSwitchViewAction() {
+    }
+
+    public void testTestToString() {
+    }
+
+    public void testDeckToString() {
+    }
+
+    public void testHandToString() {
+    }
+
+    public void testGetLastDrawnTile() {
+    }
+
+    public void testGetNumPairs() {
+    }
+
+    public void testGetPlayerID() {
+    }
+
+    public void testGetDeck() {
+    }
+
+    public void testGetPlayerOneHand() {
+    }
+
+    public void testGetCurrentDrawnTile() {
+    }
+
+    public void testGetNumSets() {
+    }
+
+    public void testGetLastDiscarded() {
+    }
+
+    public void testGetIsTurn() {
+    }
+
+    public void testSetCurrentDrawnTile() {
+    }
+
+    public void testSetLastDiscarded() {
+    }
+
+    public void testSetLastDrawnTile() {
+    }
+
+    public void testSetNumPairs() {
+    }
+
+    public void testSetNumSets() {
+    }
+
+    public void testSetPlayerID() {
+    }
+
+    public void testSetPlayerOneHand() {
+    }
+
+    public void testSetPlayerTwoHand() {
+    }
+
+    public void testSetPlayerThreeHand() {
+    }
+
+    public void testSetPlayerFourHand() {
+    }
+
+    public void testSetTurn() {
+    }
+}
