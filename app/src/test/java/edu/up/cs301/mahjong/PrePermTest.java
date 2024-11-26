0e05d48d39f6c9c9c264627d24081665866911ec
Jacqui Bouchard <boucharj27@up.edu>
2024-11-26 11:54:56 -0800

    JB: prePermTest fixes

--- app/src/test/java/edu/up/cs301/mahjong/PrePermTest.java
+++ app/src/test/java/edu/up/cs301/mahjong/PrePermTest.java
@@ -21,20 +21,21 @@ public class PrePermTest extends TestCase {
 
         MahjongGameState mgs = new MahjongGameState();
         MahjongTile[] hand = new MahjongTile[14];
+        mgs.setEmptyHand(hand);
         mgs.setCurrentDrawnTile(new SymbolsTile("Fire"));
 
         hand[0] = new HanziTile(1);
         hand[1] = new HanziTile(2);
-        hand[7] = new HanziTile(3);
+        hand[2] = new HanziTile(3);
         hand[3] = new StickTile(1);
         hand[4] = new StickTile(2);
         hand[5] = new StickTile(3);
         hand[6] = new DotsTile(1);
-        hand[2] = new DotsTile(2);
-        hand[11] = new DotsTile(3);
+        hand[7] = new DotsTile(2);
+        hand[8] = new DotsTile(3);
         hand[9] = new HanziTile(5);
         hand[10] = new HanziTile(6);
-        hand[8] = new HanziTile(7);
+        hand[11] = new HanziTile(7);
         hand[12] = new SymbolsTile("Fire");
 
         int results = mgs.prePerm(hand);
