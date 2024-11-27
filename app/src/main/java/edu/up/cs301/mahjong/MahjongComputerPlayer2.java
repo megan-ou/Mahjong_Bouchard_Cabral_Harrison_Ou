6ced9364617264a7bd8e0fc92b9ed0aea6b44b82
meganou <ou27@up.edu>
2024-11-27 15:15:19 -0800

    MO - Enable chow in smart computer player

--- app/src/main/java/edu/up/cs301/mahjong/MahjongComputerPlayer2.java
+++ app/src/main/java/edu/up/cs301/mahjong/MahjongComputerPlayer2.java
@@ -41,18 +41,6 @@ public class MahjongComputerPlayer2 extends MahjongComputerPlayer1 implements Se
 	// the most recent game state, as given to us by the CounterLocalGame
 	private MahjongGameState mgs = null;
 
-	// If this player is running the GUI, the activity (null if the player is
-	// not running a GUI).
-	private Activity activityForGui = null;
-
-	// If this player is running the GUI, the widget containing the counter's
-	// value (otherwise, null);
-	private TextView counterValueTextView = null;
-
-	// If this player is running the GUI, the handler for the GUI thread (otherwise
-	// null)
-	private Handler guiHandler = null;
-
 	private boolean hasDrawnTile = false;
 
 	//array of discard button ids
@@ -70,11 +58,12 @@ public class MahjongComputerPlayer2 extends MahjongComputerPlayer1 implements Se
 	public MahjongComputerPlayer2(String name) {
 		super(name);
 
-
 		//initialize discButtonIDArray
 		for (int i = 0; i < discButtonIDArray.length; i++) {
 			discButtonIDArray[i] = i;
 		}
+
+		hand = new MahjongTile[14];
 	}
 
     /**
@@ -110,6 +99,21 @@ public class MahjongComputerPlayer2 extends MahjongComputerPlayer1 implements Se
 			/* don't care */
 		}
 
+		switch (playerNum){
+			case 0:
+				hand = this.mgs.getPlayerOneHand();
+				break;
+			case 1:
+				hand = this.mgs.getPlayerTwoHand();
+				break;
+			case 2:
+				hand = this.mgs.getPlayerThreeHand();
+				break;
+			case 3:
+				hand = this.mgs.getPlayerFourHand();
+				break;
+		}
+
 		//Draw a tile if a tile has not yet been drawn
 		if (!hasDrawnTile) {
 			//First draw tile
@@ -118,68 +122,35 @@ public class MahjongComputerPlayer2 extends MahjongComputerPlayer1 implements Se
 
 			hasDrawnTile = true;
 		}
-		//Send a draw action to exit chow mode
-//		else if (mgs.isChowMode()) {
-//			game.sendAction(new MahjongDrawTileAction(this));
-//			hasDrawnTile = false; //just to be safe
-//		}
 
+		//Send chow action if in chow mode
+		//Just to build in a little bit of error into Smart AI, 10% chance Smart AI skips chow entirely
+		else if (mgs.isChowMode() && mgs.getPlayerID() == playerNum && !hasDrawnTile) {
+			double randNum = Math.random();
+			//chow 90% of time
+			if (randNum < 0.9) {
+				game.sendAction(new MahjongChowAction(this));
+				hasDrawnTile = true;
+			}
+			//continue 10% of the time
+			else {
+				game.sendAction(new MahjongDrawTileAction(this));
+				hasDrawnTile = false;
+			}
+		}
+
+		//If chow action is taken, finish chow turn by making a discard OR
 		//Discard a tile if a tile is drawn
 		else {
-			switch (this.mgs.getPlayerID()){
-				case 1:
-					hand = this.mgs.getPlayerOneHand();
-					break;
-				case 2:
-					hand = this.mgs.getPlayerTwoHand();
-					break;
-				case 3:
-					hand = this.mgs.getPlayerThreeHand();
-					break;
-				case 4:
-					hand = this.mgs.getPlayerFourHand();
-					break;
-			}
 			discardHelper();
 			hasDrawnTile = false;
-
 		}
 
 
 
 	}
 
-
 	/**
-	 * sets the counter value in the text view
-	 *  */
-	private void updateDisplay() {
-//		// if the guiHandler is available, set the new counter value
-//		// in the counter-display widget, doing it in the Activity's
-//		// thread.
-//		if (guiHandler != null) {
-//			guiHandler.post(
-//					new Runnable() {
-//						public void run() {
-//						if (counterValueTextView != null && currentGameState != null) {
-//							//counterValueTextView.setText("" + currentGameState.getCounter());
-//						}
-//					}});
-//		}
-	}
-
-	/**
-	 * Tells whether we support a GUI
-	 *
-	 * @return
-	 * 		true because we support a GUI
-	 */
-	public boolean supportsGui() {
-		return true;
-	}
-
-
-		/**
 	 * code to send a discard
 	 */
 	protected void discardHelper() {
